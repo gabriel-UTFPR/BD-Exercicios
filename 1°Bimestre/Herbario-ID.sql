e81b733a7aa6f1f2eb1f40c4693e5f9c7a3c6a0b
Gabriel Soares <gabriel.2010@alunos.utfpr.edu.br>
2026-04-19 16:23:54 -0300

    19/04/2026

--- 1°Bimestre/Herbario-ID.sql
+++ 1°Bimestre/Herbario-ID.sql
@@ -1,17 +1,59 @@
-
-DROP TABLE ESPECIME,ESPECIE,ETIQUETA,ARMARIO,EXPEDICAO,PESQUISADOR;
-
-CREATE TABLE ESPECIME(
- tombo integer,
- data_coleta date,
- maturacao varchar(200),
- primary key(tombo)
-);
+DROP TABLE IF EXISTS ETIQUETA, EXPEDICAO_PESQUISADOR, ESPECIME, EXPEDICAO, PESQUISADOR, ESPECIE, ARMARIO;
 
 CREATE TABLE ESPECIE(
- nome_cientifico varchar(200),
- nome_popular varchar(200),
- familia_botanica varchar(200),
- primary ke
-
+  nome VARCHAR(200),
+  nome_popular VARCHAR(200),
+  familia VARCHAR(200),
+  PRIMARY KEY(nome)
 );
+
+CREATE TABLE ARMARIO(
+  num INTEGER,
+  capacidade_maxima INTEGER,
+  PRIMARY KEY(num)
+);
+
+CREATE TABLE PESQUISADOR(
+  cpf CHAR(11),
+  nome VARCHAR(200),
+  instituicao VARCHAR(200),
+  PRIMARY KEY (cpf)
+);
+
+CREATE TABLE EXPEDICAO(
+  nome VARCHAR(200),
+  bioma VARCHAR(200),
+  data_expedicao DATE,
+  PRIMARY KEY (nome)
+);
+
+CREATE TABLE ESPECIME(
+  tombo INTEGER,
+  data_coleta DATE,
+  estagio VARCHAR(200),
+  nome_especie VARCHAR(200),
+  nome_expedicao VARCHAR(200),
+  num_armario INTEGER,
+  PRIMARY KEY(tombo),
+  FOREIGN KEY(nome_especie) REFERENCES ESPECIE(nome),
+  FOREIGN KEY(nome_expedicao) REFERENCES EXPEDICAO(nome),
+  FOREIGN KEY(num_armario) REFERENCES ARMARIO(num)
+);
+
+CREATE TABLE EXPEDICAO_PESQUISADOR(
+   cpf CHAR(11),
+   nome_expedicao VARCHAR(200),
+   funcao VARCHAR(200),
+   PRIMARY KEY (cpf, nome_expedicao),
+   FOREIGN KEY(cpf) REFERENCES PESQUISADOR(cpf),
+   FOREIGN KEY(nome_expedicao) REFERENCES EXPEDICAO(nome)
+);
+
+CREATE TABLE ETIQUETA(
+   codigo_hexadecimal CHAR(100),
+   fabricante VARCHAR(200),
+   tombo_especime INTEGER,
+   PRIMARY KEY (codigo_hexadecimal),
+   UNIQUE (tombo_especime),
+   FOREIGN KEY (tombo_especime) REFERENCES ESPECIME(tombo)
+)
